def624fcf92b6536720595b4832c2a70dd577ded
modulos <modulos@protonmail.com>
2018-04-03 11:00:56 +0200

    Add typography.js, remove index.css

---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from "typography";
+
+const typography = new Typography({
+  baseFontSize: "20px",
+  bodyFontFamily: ['Libre Franklin', 'Helvetica Neue', 'sans-serif'],
+  headerFontFamily: ['Georgia', 'serif'],
+});
+
+export default typography;
