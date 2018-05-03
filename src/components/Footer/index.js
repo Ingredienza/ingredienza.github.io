c8ae94214d0faa026c5a709319e0da5593f6d433
modulos <modulos@protonmail.com>
2018-05-03 16:19:11 +0200

    Fix footer position

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link';
 import logo from './logo.png';
 
 const Footer = () =>
-  <footer style={{ gridArea: 'footer', zIndex: 10000 }}>
+  <footer style={{ gridArea: 'footer', zIndex: 10000, justifySelf: 'end', }}>
     <img src={logo} alt="Ingredienza Logo" /> 
   </footer>
 
