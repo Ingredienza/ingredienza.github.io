00c0ec72ee4adf779a1a0879fccec00e6e117c94
modulos <modulos@protonmail.com>
2018-05-03 14:55:37 +0200

    Move ravioli to new table

--- src/pages/products/ravioli.js
+++ src/pages/products/ravioli.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import Img from 'gatsby-image';
+import ProductTable from '../../components/ProductTable';
 
 const RavioliPage = ({ data }) => (
   <div style={{ gridArea: 'content' }}>
@@ -29,243 +30,192 @@ const RavioliPage = ({ data }) => (
       Teigwaren-Manufaktur werden Kreativität und Exklusivität grossgeschrieben.
       Dementsprechend variantenreich ist unser Sortiment an Ravioli. 
     </p>
-    <p><b>...mit Kr&auml;utern und Gem&uuml;se</b></p>    
-    <table border="0" bordercolor="#000000" cellpadding="2" cellspacing="2" width="660">
-      <colgroup>
-        <col width="100" />
-        <col width="350" />
-        <col width="100" />
-        <col width="100" />
-      </colgroup>
-      <tr>
-        <th>Art.Nr</th>
-        <th>Produkt</th>
-        <th>Einheit</th>
-        <th>Fr. / kg</th>
-      </tr>
-      <tr>
-        <td>BITA007</td>
-        <td>Bio Auberginen-Triangoli im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 23.50</td>
-      </tr>
-      <tr>
-        <td>B50002</td>
-        <td>Bio Baumnuss-Mohn-Zitrone-Rechtecke im Spinatteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 24.50</td>
-      </tr>
-      <tr>
-        <td>BITA003</td>
-        <td>Bio Ricotta-Rucola-Quadrolini im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 23.50</td>
-      </tr>
-      <tr>
-        <td>B30001</td>
-        <td>Bio Ricotta-Spinat-Mezzelune 3-farbig gemischt</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 21.--</td>
-      </tr>
-      <tr>
-        <td>BITA005</td>
-        <td>Bio Ricotta-Zitronen-Rondellen im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 22.50</td>
-      </tr>
-      <tr>
-        <td>40042</td>
-        <td>All'arrabbiata-Quadrolini grün-weiss gestreift</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 22.50</td>
-      </tr>
-      <tr>
-        <td>ITA004</td>
-        <td>Artischocken-Triangoli im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 22.50</td>
-      </tr>
-      <tr>
-        <td>20001</td>
-        <td>Basilikum-Mezzelune Grande im Dinkelteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 18.50</td>
-      </tr>
-      <tr>
-        <td>40002</td>
-        <td>Basilikum-Triangoli im Safranteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 22.50</td>
-      </tr>
-      <tr>
-        <td>10003</td>
-        <td>Basilikum-Raviolini im Safranteig<br /> Ideal als Suppeneinlage 2.0 x 2.5cm.</td>
-        <td>&cent; &agrave; 3kg TK</td>
-        <td>Fr. 13.50</td>
-      </tr>
-      <tr>
-        <td>10006</td>
-        <td>Basilikum-Tortellini im Eierteig</td>
-        <td>&cent; &agrave; 5kg TK</td>
-        <td>Fr. 13.50</td>
-      </tr>
-      
-      <tr>
-        <td>20002</td>
-        <td>Ricotta-Kr&auml;uter-Quadrolini rot weiss gestreift</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 23.50</td>
-      </tr>
-      <tr>
-        <td>40006</td>
-        <td>Pelati-Rondellen im Peterliteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 15.50</td>
-      </tr>
-      <tr>
-        <td>ITA 008</td>
-        <td>Pomodori secchi-Ricotta-Triangoli im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 20.50</td>
-      </tr>
-      <tr>
-        <td>ITA008M</td>
-        <td>Pomodori secchi-Mascarpone-Rondellen im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 22.50</td>
-      </tr>
-      <tr>
-        <td>50001</td>
-        <td>Ratatouille-Rechtecke im Dillteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 18.50</td>
-      </tr>     
-    </table>
-
+    <p><b>...mit Kräutern und Gemüse</b></p>    
+    <ProductTable
+      showHeader={true}
+      products={[
+        {
+          articleNr: 'BITA007',
+          description: `Bio Auberginen-Triangoli im Eierteig`,
+          unit: '2kg TK',
+          price: '23.50',
+        },
+        {
+          articleNr: 'B50002',
+          description: `Bio Baumnuss-Mohn-Zitrone-Rechtecke im Spinatteig`,
+          unit: '2kg TK',
+          price: '24.50',
+        },
+        {
+          articleNr: 'BITA003',
+          description: `Bio Ricotta-Rucola-Quadrolini im Eierteig`,
+          unit: '2kg TK',
+          price: '23.50',
+        },
+        {
+          articleNr: 'B30001',
+          description: `Bio Ricotta-Spinat-Mezzelune 3-farbig gemischt`,
+          unit: '2kg TK',
+          price: '21.--',
+        },
+        {
+          articleNr: 'BITA005',
+          description: `Bio Ricotta-Zitronen-Rondellen im Eierteig`,
+          unit: '2kg TK',
+          price: '22.50',
+        },
+        {
+          articleNr: '40042',
+          description: `All' arrabbiata-Quadrolini grün-weiss gestreift`,
+          unit: '2kg TK',
+          price: '23.--',
+        },
+        {
+          articleNr: 'ITA004',
+          description: `Artischocken-Triangoli im Eierteig`,
+          unit: '2kg TK',
+          price: '22.50',
+        },
+        {
+          articleNr: '20001',
+          description: `Basilikum-Mezzelune Grande im Dinkelteig`,
+          unit: '2kg TK',
+          price: '18.50',
+        },
+        {
+          articleNr: '40002',
+          description: `Basilikum-Triangoli im Safranteig`,
+          unit: '2kg TK',
+          price: '22.50',
+        },
+        {
+          articleNr: '10003',
+          description: `Basilikum-Raviolini im Safranteig\nIdeal als Suppeneinlage 2.0 x 2.5cm.`,
+          unit: '2kg TK',
+          price: '13.50',
+        },
+        {
+          articleNr: '10006',
+          description: `Basilikum-Tortellini im Eierteig`,
+          unit: '2kg TK',
+          price: '13.50',
+        },
+        {
+          articleNr: '20002',
+          description: `Ricotta-Kräuter-Quadrolini rot-weiss gestreift`,
+          unit: '2kg TK',
+          price: '23.50',
+        },
+        {
+          articleNr: '40006',
+          description: `Pelati-Rondellen im Peterliteig`,
+          unit: '2kg TK',
+          price: '15.50',
+        },
+        {
+          articleNr: 'ITA008',
+          description: `Pomodori secchi-Ricotta-Triangoli im Eierteig`,
+          unit: '2kg TK',
+          price: '20.50',
+        },
+        {
+          articleNr: 'ITA008M',
+          description: `Pomodori secchi-Mascarpone-Rondellen im Eierteig`,
+          unit: '2kg TK',
+          price: '22.50',
+        },
+        {
+          articleNr: '50001',
+          description: `Ratatouille-Rechtecke im Dillteig`,
+          unit: '2kg TK',
+          price: '18.50',
+        },
+      ]}
+    />
     <p><b>... mit Fleisch und Fisch</b></p>
-    <table border="0" bordercolor="#000000" cellpadding="2" cellspacing="2" width="660">
-      <colgroup>
-        <col width="100" />
-        <col width="350" />
-        <col width="100" />
-        <col width="100" />
-      </colgroup>
-      <tr>
-        <th>Art.Nr</th>
-        <th>Produkt</th>
-        <th>Einheit</th>
-        <th>Fr. / kg</th>
-      </tr>
-      <tr>
-        <td>40004</td>
-        <td>
-          Brasato-Quadrolini-im Spinatteig<br />
-          (Highland Rindfleisch von der Powerfarm in Lyssach)
-        </td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 23.50</td>
-      </tr>
-      <tr>
-        <td>50011</td>
-        <td>
-          Ricotta-Rauchlachs-Meerettich-Fischli im Dillteig<br />
-          (Alaska Wildlachs von Sami Gugger)
-        </td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 24.50</td>
-      </tr>
-      <tr>
-        <td>40030</td>
-        <td>Rohschinken-Quadroloni im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 23.50</td>
-      </tr>
-    </table>
-
+    <ProductTable
+      showHeader={true}
+      products={[
+        {
+          articleNr: '40004',
+          description: `Ratatouille-Rechtecke im Dillteig\n(Highland Rindfleisch von der Powerfarm in Lyssach)`,
+          unit: '2kg TK',
+          price: '23.50',
+        },
+        {
+          articleNr: '50011',
+          description: `Ricotta-Rauchlachs-Meerettich-Fischli im Dillteig\n(Alaska Wildlachs von Sami Gugger)`,
+          unit: '2kg TK',
+          price: '24.50',
+        },
+        {
+          articleNr: '40030',
+          description: `Rohschinken-Quadroloni im Eierteig`,
+          unit: '2kg TK',
+          price: '23.50',
+        },
+      ]}
+    />
     <p><b>...mit Pilzen</b></p>
-
-    <table border="0" bordercolor="#000000" cellpadding="2" cellspacing="2" width="660">
-      <colgroup>
-        <col width="100" />
-        <col width="350" />
-        <col width="100" />
-        <col width="100" />
-      </colgroup>
-      <tr>
-        <th>Art.Nr</th>
-        <th>Produkt</th>
-        <th>Einheit</th>
-        <th>Fr. / kg</th>
-      </tr>
-      <tr>
-        <td>40005</td>
-        <td>Steinpilz-Frischk&auml;se-Rondellen im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 31.50</td>
-      </tr>
-      <tr>
-        <td>50021</td>
-        <td>Butterpilz-Rondellen im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 19.50</td>
-      </tr>
-    </table>
-
-    <p><b>... mit K&auml;se</b></p>
-
-    <table border="0" bordercolor="#000000" cellpadding="2" cellspacing="2" width="660">
-      <colgroup>
-        <col width="100" />
-        <col width="350" />
-        <col width="100" />
-        <col width="100" />
-      </colgroup>
-      <tr>
-        <th>Art.Nr</th>
-        <th>Produkt</th>
-        <th>Einheit</th>
-        <th>Fr. / kg</th>
-      </tr>
-      <tr>
-        <td>B50004</td>
-        <td>Bio Ricotta-Balsamico-Rondellen im Peperoniteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 22.50</td>
-      </tr>
-      <tr>
-        <td>40001</td>
-        <td>Ricotta-Parmesan-Rondellen im Paprikateig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 18.50</td>
-      </tr>
-      <tr>
-        <td>40018</td>
-        <td>Geissk&auml;se-Pomodori secchi-Quadrolini im Schnittlauchteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 23.50</td>
-      </tr>
-      <tr>
-        <td>40021</td>
-        <td>Ziegenfrischk&auml;se-Honig-Thymian-Raviolo Grande</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 24.50</td>
-      </tr>
-      <tr>
-        <td>40033</td>
-        <td>
-          Quattro-Formaggi-Mezzelune Grande im Eierteig <br />
-          (Ricotta, Gorgonzola, Taleggio und Parmesan)
-        </td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 22.50</td>
-      </tr>     
-      <tr>
-        <td>40044</td>
-        <td>Feigen-Pecorino-Rechtecke im Eierteig</td>
-        <td>&cent; &agrave; 2kg TK</td>
-        <td>Fr. 26.50</td>
-      </tr>     
-    </table>
+    <ProductTable
+      showHeader={true}
+      products={[
+        {
+          articleNr: '40005',
+          description: `Steinpilz-Frischkäse-Rondellen im Eierteig`,
+          unit: '2kg TK',
+          price: '31.50',
+        },
+        {
+          articleNr: '50021',
+          description: `Butterpilz-Rondellen im Eierteig`,
+          unit: '2kg TK',
+          price: '19.50',
+        },
+      ]}
+    />
+    <p><b>... mit Käse</b></p>
+    <ProductTable
+      showHeader={true}
+      products={[
+        {
+          articleNr: 'B50004',
+          description: `Bio Ricotta-Balsamico-Rondellen im Peperoniteig`,
+          unit: '2kg TK',
+          price: '22.50',
+        },
+        {
+          articleNr: '40001',
+          description: `Ricotta-Parmesan-Rondellen im Paprikateig`,
+          unit: '2kg TK',
+          price: '18.50',
+        },
+        {
+          articleNr: '40018',
+          description: `Geisskäse-Pomodori secchi-Quadrolini im Schnittlauchteig`,
+          unit: '2kg TK',
+          price: '23.50',
+        },
+        {
+          articleNr: '40021',
+          description: `Ziegenfrischkäse-Honig-Thymian-Raviolo Grande`,
+          unit: '2kg TK',
+          price: '24.50',
+        },
+        {
+          articleNr: '40033',
+          description: `Quattro-Formaggi-Mezzelune Grande im Eierteig\n(Ricotta, Gorgonzola, Taleggio und Parmesan)`,
+          unit: '2kg TK',
+          price: '22.50',
+        },
+        {
+          articleNr: '40044',
+          description: `Feigen-Pecorino-Rechtecke im Eierteig`,
+          unit: '2kg TK',
+          price: '26.50',
+        },
+      ]}
+    />
   </div>
 )
 
